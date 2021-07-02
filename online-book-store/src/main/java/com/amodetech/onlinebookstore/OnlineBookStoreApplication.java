a6b6b1835849d43e4a8c953b44b3fa4e799837dd
vishal <vishal.amode@gmail.com>
2021-07-03 01:06:49 +0530

    inital commit created a spring boot backend application and configure the application properties

---
+++ online-book-store/src/main/java/com/amodetech/onlinebookstore/OnlineBookStoreApplication.java
@@ -0,0 +1,15 @@
+package com.amodetech.onlinebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookStoreApplication {
+	
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookStoreApplication.class, args);
+	}
+
+}
