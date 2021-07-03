774c5116227cfa0ee30d79486b07b42e058c8da0
vishal <vishal.amode@gmail.com>
2021-07-04 01:29:07 +0530

    Angular Project committed feature 1

---
+++ angular-bookstore/src/app/common/book.ts
@@ -0,0 +1,13 @@
+export class Book {
+
+    sku: String;
+    name: String;
+    description: String;
+    unitPrice: number;
+    imageUrl: String;
+    active: boolean;
+    unitsInStock: number;
+    createdOn: Date;
+    updatedOn: Date;
+
+}
